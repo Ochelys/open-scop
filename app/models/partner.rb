1ddc4eae83b3dd8b70702853eda013996db84686
Dmaury <david.maury0711@gmail.com>
2020-11-18 09:55:17 +0100

    Calculates shares and percentage

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -2,4 +2,11 @@ class Partner < ActiveRecord::Base
     belongs_to :company
     has_many :share_movement
     
+    def shares?
+        ShareMovement.where(partner_id: self.id).sum(:quantity)
+    end
+
+    def percent
+      ShareMovement.where(partner_id: self.id).sum(:quantity)*100/ShareMovement.all.sum(:quantity)
+    end
 end
